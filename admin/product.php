0852018f58367c975c0540ecebb315dc00701bb2
Trungtt1801 <165350061+Trungtt1801@users.noreply.github.com>
2024-11-21 11:04:17 +0700

    fix php

--- admin/product.php
+++ admin/product.php
@@ -30,12 +30,20 @@
             <form action="#">
                 <div class="flex">
                     <div class="inputbox">
+                        <p>product id (required)</p>
+                        <input type="text" class="boxinput" placeholder="enter product id">
+                    </div>
+                    <div class="inputbox">
                         <p>product name (required)</p>
                         <input type="text" class="boxinput" placeholder="enter product name">
                     </div>
                     <div class="inputbox">
                         <p>product price (required)</p>
                         <input type="number" class="boxinput" placeholder="enter product price">
+                    </div>
+                    <div class="inputbox">
+                        <p>product sale price (required)</p>
+                        <input type="number" class="boxinput" placeholder="enter product sale price">
                     </div>
                     <div class="inputbox">
                         <p>image 01 (required)</p>
@@ -50,6 +58,25 @@
                         <input type="file" name="img3" id=""class="boxinput">
                     </div>
                     <div class="inputbox">
+                        <p>image 04 (required)</p>
+                        <input type="file" name="img3" id=""class="boxinput">
+                    </div>
+                    <div class="inputbox">
+                        <p>product category  (required)</p>
+                        <select name="" id="">
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                        </select>
+                    </div>
+                    <div class="inputbox">
+                        <p>product status (required)</p>
+                        <select name="" id="">
+                            <option value="">in stock</option>
+                            <option value="">sold out</option>
+                        </select>
+                    </div>
+                    <div class="inputbox">
                         <p>product details (required)</p>
                         <input type="text" class="boxinput" placeholder="enter product detail">
                     </div>
@@ -62,19 +89,23 @@
             <table>
                 <thead>
                     <tr>
+                    <th>Iddm</th>
                     <th>Image</th>
                     <th>Name</th>
                     <th>Price</th>
                     <th>Detail</th>
+                    <th>status</th>
                     <th>Custom</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
+                        <td>1</td>
                         <td><img src="https://product.hstatic.net/1000306633/product/truoc_a5458ac1af084d18bcb742c0858a9cec.jpg" alt="" srcset=""></td>
                         <td>HADES FLEURS RED TEE</td>
-                        <td>450.000 <span>VND</span></td>
+                        <td>old: 450.000 <span>VND</span><br> new: 350.000 <span>VND</span></td>
                         <td>THÔNG TIN • Form basic mới • Hoạ tiết chữ in kéo lụa full bản ở mặt trước và mặt sau áo • 2 charm FLOWER được đính bất đối xứng trên áo, có thể tách rời • Logo Hades thêu nổi cùng màu ở tay áo • Chất liệu: Cotton 2 chiều Lưu ý: Màu sắc sản phẩm có thể khác trên ảnh do điều kiện ánh sáng khi chụp. MODEL: 1m72 50kg, 73-65-91 mặc size M Vận chuyển từ 2-3 ngày. Thiết kế và sản xuất bởi HADES</td>
+                        <td>in stock</td>
                         <td>
                             <div class="iconsp">
                                 <a href="#"class="fas fa-edit"></a>
@@ -83,10 +114,12 @@
                         </td>
                     </tr>
                     <tr>
+                        <td>1</td>
                         <td><img src="https://product.hstatic.net/1000306633/product/dsc09856_1d3a8091abae4a6ab6d4e9256df2b4e9.jpg" alt="" srcset=""></td>
                         <td>HADES CERISE BOW TEE</td>
                         <td>450.000 <span>VND</span></td>
                         <td>THÔNG TIN • Form basic mới • Các chi tiết nơ lớn nhỏ được đính bất đối xứng ở mặt trước áo cùng hoạ tiết chữ in kéo lụa • Logo Hades thêu nổi cùng màu ở tay áo • Chất liệu: Cotton 2 chiều MODEL: 1m72 50kg, 73-65-91 mặc size M Vận chuyển từ 2-3 ngày. Thiết kế và sản xuất bởi HADES</td>
+                        <td>in stock</td>
                         <td>
                             <div class="iconsp">
                                 <a href="#"class="fas fa-edit"></a>
@@ -95,10 +128,12 @@
                         </td>
                     </tr>
                     <tr>
+                        <td>1</td>
                         <td><img src="https://tse1.mm.bing.net/th?id=OIP.Qbm9P45SHx61UH5kPnoONAHaJ4&pid=Api" alt="" srcset=""></td>
                         <td>HADES LOVE HARDER TEE</td>
                         <td>450.000 <span>VND</span></td>
                         <td>• Form basic mới • Graphic mặt trước áo sử dụng kết hợp kỹ thuật in kéo lụa và in nổi • Charm bông hình môi được đính ở mặt trước áo • Logo Hades thêu nổi ở tay áo • Chất liệu: Cotton 2 chiều Lưu ý: Màu sắc sản phẩm có thể khác trên ảnh do điều kiện ánh sáng khi chụp. MODEL nữ: 1m66, 81-60-87 mặc size M MODEL nam: 1m84, 98-79-97 mặc size XL Vận chuyển từ 2-3 ngày. Thiết kế và sản xuất bởi HADES</td>
+                        <td>in stock</td>
                         <td>
                             <div class="iconsp">
                                 <a href="#"class="fas fa-edit"></a>
@@ -107,10 +142,12 @@
                         </td>
                     </tr>
                     <tr>
+                        <td>2</td>
                         <td><img src="https://sweetiee.click/ecommerce/uploaded_img/hd_t53977_30e888701c6d4ae6ba5aebd2fe06c860.webp" alt="" srcset=""></td>
                         <td>HADES MASCOT BAG</td>
                         <td>400.000 <span>VND</span></td>
                         <td>THÔNG TIN • Túi đeo chéo hình mascot Hades • Có khoá kéo kim loại chắc chắn • Chống thấm nước • Các chi tiết trên mặt được thêu • Chất liệu: Simili • Kích thước: ngang 19,5cm x cao 32cm' Lưu ý: Màu sắc sản phẩm có thể khác trên ảnh do điều kiện ánh sáng khi chụp. Vận chuyển từ 2-3 ngày. Thiết kế và sản xuất bởi HADES</td>
+                        <td>in stock</td>
                         <td>
                             <div class="iconsp">
                                 <a href="#"class="fas fa-edit"></a>
@@ -119,10 +156,12 @@
                         </td>
                     </tr>
                     <tr>
+                        <td>3</td>      
                         <td><img src="https://sweetiee.click/ecommerce/uploaded_img/hades_001666_821e1b9693b3470882ca5ddfc72e4fac.webp" alt="" srcset=""></td>
                         <td>TORNADO ROUTE PANTS</td>
                         <td>790.000 <span>VND</span></td>
                         <td>THÔNG TIN • Quần jeans form suông, cạp quần trễ, độ dài phủ gót • Xử lý wash tạo hiệu ứng riêng biệt • Nổi bật với các chi tiết giả lai và ráp nối vải toàn bộ mặt trước và sau quần • Chi tiết wash rách trên đùi và gấu quần • Có hai túi mổ sâu phía trước và hai túi phía sau kèm hoạ tiết thêu • Chất liệu: Jean Vận chuyển từ 2-3 ngày. Thiết kế và sản xuất bởi HADES</td>
+                        <td>in stock</td>
                         <td>
                             <div class="iconsp">
                                 <a href="#"class="fas fa-edit"></a>
