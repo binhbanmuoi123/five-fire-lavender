ddcbb808eacc6ee96c9192a06b9e8028da4eaea5
xhuong278 <huongphxps39408@gmail.com>
2024-11-22 13:09:20 +0700

    up

---
+++ admin/views/v_admins.php
@@ -0,0 +1,49 @@
+<main>
+        <div class="container">
+            <h1> ADMIN ACCOUNTS</h1>
+            <div class="box-container">
+                <div class="box-c">
+                    <p>add new admin</p>
+                    <div class="div-button">
+                        <button-r>Register admin</button-r>
+                    </div>
+                </div>
+                <div class="box-c">
+                    <p>admin id : <span>1</span></p>
+                    <p>admin name : <span>admin</span></p>
+                    <div class="div-button">
+                        <button-1>Update</button-1>
+                        <button-2>Delete</button-2>
+                    </div>
+                </div>
+                <div class="box-c">
+                    <p>admin id : <span>2</span></p>
+                    <p>admin name : <span>admin1</span></p>
+                    <div class="div-button">
+                        <button-d>Delete</button-d>
+                    </div>
+                </div>
+                <div class="box-c">
+                    <p>admin id : <span>3</span></p>
+                    <p>admin name : <span>vinh</span></p>
+                    <div class="div-button">
+                        <button-d>Delete</button-d>
+                    </div>
+                </div>
+                <div class="box-c">
+                    <p>admin id : <span>4</span></p>
+                    <p>admin name : <span>trung</span></p>
+                    <div class="div-button">
+                        <button-d>Delete</button-d>
+                    </div>
+                </div>
+                <div class="box-c">
+                    <p>admin id : <span>5</span></p>
+                    <p>admin name : <span>huong</span></p>
+                    <div class="div-button">
+                        <button-d>Delete</button-d>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
---
+++ admin/views/v_users.php
@@ -0,0 +1,32 @@
+<main>
+        <div class="container">
+            <h1> USER ACCOUNTS</h1>
+            <div class="box-container">
+                <div class="box-c">
+                    <p>user id : <span>1</span></p>
+                    <p>user name : <span>tie</span></p>
+                    <p>email : <span>huongcutee2005@gmail.com</span></p>
+                    <div class="div-button">
+                        <button-d>Delete</button-d>
+                    </div>
+                </div>
+                <div class="box-c">
+                    <p>user id : <span>2</span></p>
+                    <p>user name : <span>trương trí trung</span></p>
+                    <p>email : <span>truongtritrung1801@gmail.com</span></p>
+                    <div class="div-button">
+                        <button-d>Delete</button-d>
+                    </div>
+                </div>
+                <div class="box-c">
+                    <p>user id : <span>3</span></p>
+                    <p>user name : <span>vinh</span></p>
+                    <p>email : <span>123trongvinh456@gmail.com</span></p>
+                    <div class="div-button">
+                        <button-d>Delete</button-d>
+                    </div>
+                </div>              
+                </div>
+            </div>
+        </div>
+    </main>
